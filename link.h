274a1c3f1dea552c06a713b66e28604e2fef0aed
JoshuaBonn1 <bonn7152@vandals.uidaho.edu>
2017-03-04 23:39:09 -0800

    Create link.h

---
+++ link.h
@@ -0,0 +1,72 @@
+/*  link.h
+ */
+
+// This is a class for a sorted linked list of integers.
+
+#include <iostream>
+
+using namespace std;
+
+class LinkedList
+{
+private:
+
+  struct node
+  {
+     int info;
+     node * next;
+  };
+
+  typedef node * nodeptr;
+
+  nodeptr start;
+
+  int count;
+
+
+public:
+
+       // Constructor
+
+   LinkedList()
+   {
+      start = NULL;
+      count = 0;
+   }
+
+       // Destructor
+
+   ~LinkedList()
+   {
+      nodeptr p = start, n;
+
+      while (p != NULL)
+      {
+         n = p;
+         p = p->next;
+         delete n;
+      }
+   }
+
+   //Adds node to the front of list
+   void AddNode(int x);
+
+   //Deletes the first node
+   void DeleteNode();
+
+   //Returns info of the first node on the list
+   int InfoAtTop();
+
+    // Output the values in the nodes, one integer per line.
+
+   void ListNodes();
+
+    // Return 1 if there in a node in the list with the value x.
+    // Return 0 if there is NOT a node in the list with the value x.
+
+   int IsInList(int x);
+
+    // Return a count of the number of nodes in the list.
+
+   int Size();
+};
