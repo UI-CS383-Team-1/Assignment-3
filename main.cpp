cc0cde5b79d2164def4ac83bd7269a3f0778a2e5
JoshuaBonn1 <bonn7152@vandals.uidaho.edu>
2017-03-04 23:47:44 -0800

    Added sample code to test linked list

--- main.cpp
+++ main.cpp
@@ -4,9 +4,42 @@
 */
 
 #include <iostream>
-#include "list.h"
+#include "link.h"
 using namespace std;
 
 int main() {
+  LinkedList list1;
+  
+  //Add some initial nodes
+  list1.AddNode(3);
+  list1.AddNode(5);
+  
+  cout << "Initial contents of list1:" << endl;
+  list1.ListNodes();
+  
+  cout << "Is 3 in the list? ";
+  if(list1.IsInList(3))
+    cout << "yes" << endl;
+  else
+    cout << "no" << endl;
+  
+  //Add a few more nodes
+  list1.AddNode(1);
+  list1.AddNode(4);
+  
+  cout << "Contents of list1 after adding:" << end;
+  list1.ListNodes();
+  
+  //Delete a node
+  list1.DeleteNode();
+  cout << "Deleted" << endl;
+  
+  cout << "Contents of list1 after deleting:" << end;
+  list1.ListNodes();
+  
+  int info = list1.InfoAtTop();
+  cout << "Top node: " << info << endl;
+
+  return 0;
 
 }
