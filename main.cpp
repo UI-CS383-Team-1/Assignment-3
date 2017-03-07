d956c49d9deebd8aafff559be45bb2e178e66324
JoshuaBonn1 <bonn7152@vandals.uidaho.edu>
2017-03-07 08:02:49 -0800

    Added stack output

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 */
 
 #include <iostream>
-#include "link.h"
+#include "stack.h"
 using namespace std;
 
 int main() {
@@ -42,6 +42,37 @@ int main() {
 
   cout << "list1 Average: " << list1.Average() << endl;
   cout << "list1 Standard Deviation: " << list1.StandardDeviation() << endl;  
+  
+  Stack iStack;
+
+  cout << "Pushing integers onto iStack" << endl;
+
+  for( int i = 0 ; i < 5 ; i++ ) {
+    iStack.Push(i);       // push items onto the stack
+    cout << i << ' ';
+  }
+  cout << endl;
+
+  cout << "Contents of iStack" << endl;
+  iStack.Print();          // output the stack contents
+
+  iStack.Reverse();
+  cout << "Contents of iStack after reverse"<< endl;
+  iStack.Print();
+
+  cout << endl << "Popping integers from iStack" << endl;
+
+  while( !iStack.IsEmpty() )
+    cout << iStack.Pop() << ' ';
+
+  cout << endl;
+  iStack.Print();          // output the stack contents
+
+
+  if( iStack.IsEmpty() )
+    cout << "The stack is empty" << endl;
+  else
+    cout << "The stack is not empty" << endl;
 
   return 0;
 
